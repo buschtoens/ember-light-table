32cfda136b3d888934d4b7fcc7bdaf33970a4f78
Jan Buschtöns <buschtoens@gmail.com>
2016-10-05 16:05:02 +0200

    stash: add examples

---
+++ tests/dummy/app/controllers/scroll-position.js
@@ -0,0 +1,50 @@
+import Ember from 'ember';
+import TableController from './table';
+
+const {
+  computed
+} = Ember;
+
+export default TableController.extend({
+  columns: computed(function() {
+    return [{
+      label: 'Avatar',
+      valuePath: 'avatar',
+      width: '60px',
+      sortable: false,
+      cellComponent: 'user-avatar'
+    }, {
+      label: 'First Name',
+      valuePath: 'firstName',
+      width: '150px'
+    }, {
+      label: 'Last Name',
+      valuePath: 'lastName',
+      width: '150px'
+    }, {
+      label: 'Address',
+      valuePath: 'address'
+    }, {
+      label: 'State',
+      valuePath: 'state'
+    }, {
+      label: 'Country',
+      valuePath: 'country'
+    }];
+  }),
+
+  maxIndex: computed('table.visibleRows.length', function() {
+    return this.get('table.visibleRows.length') - 1;
+  }),
+
+  targetIndex: null,
+  targetRow: null,
+  currentRow: null,
+
+  actions: {
+    scrollTo(index) {
+      const row = this.get('table.visibleRows').objectAt(index);
+      this.set('targetRow', row);
+    }
+  }
+});
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -11,6 +11,7 @@ Router.map(function() {
   this.route('expandable');
   this.route('selectable');
   this.route('resizable');
+  this.route('scroll-position');
 });
 
 export default Router;
